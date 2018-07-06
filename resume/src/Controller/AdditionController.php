738cfd97a2f0dbbfdc4276332e8a05bb33ba6270
Mahtab Alam <mahtab0032@gmail.com>
2018-07-06 17:57:00 +0530

    Add files via upload

--- resume/src/Controller/AdditionController.php
+++ resume/src/Controller/AdditionController.php
@@ -8,6 +8,7 @@
 namespace Drupal\resume\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
 
 /**
  * Controller routines for page example routes.
@@ -16,14 +17,31 @@ class AdditionController extends ControllerBase {
 
   public function add($first, $second, $third) {
 
-    $total[] = $first + $second + $third;
+    $total = array('first' => $first);
+		print 'hello';
+		print_r($total);
+		$node = Node::load($first);
+		print 'hello2';
+		print_r($node);
+		print '===========================';
+		
+		// Get a node storage object.
+$node_storage = \Drupal::entityManager()->getStorage('node');
+
+// Load a single node.
+print_r($node_storage->load($first));
+
+exit;
 
     $render_array['addition_arguments'] = array(
       // The theme function to apply to the #items
-      '#theme' => 'item_list',
+      //'#theme' => 'item_list',
       // The list itself.
-      '#items' => $total ,
-      '#title' => $this->t('Addition of 3 values'),
+      //'#items' => $total ,
+      //'#title' => $this->t('Addition of 3 values'),
+			'#theme' => 'my_template',
+      '#test_var' => $this->t('Create PDF'),
+			'#doubles' => $total,
     );
     return $render_array;
   }
